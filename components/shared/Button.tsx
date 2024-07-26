5e2922c1c78ea8e70b5365e860e2c5b675403e01
Francis Matthew <matthewabiodun2001@gmail.com>
2024-07-26 04:06:22 +0100

    enhanced the button componenents

--- components/shared/Button.tsx
+++ components/shared/Button.tsx
@@ -5,8 +5,8 @@ import { ImSpinner2 } from "react-icons/im"
 const buttonStyles = cva("rounded-lg font-bold flex gap-2 w-full disabled:opacity-50 justify-center items-center outline-none", {
     variants: {
       intent: {
-        primary: "bg-[#393ECC] border-[#393ECC] border text-white hover:bg-white hover:text-[#393ECC] border-[#393ECC]",
-        outline: "bg-transparent border border-[#393ECC] text-[#393ECC]",
+        primary: "bg-[#0065C2] border-[#0065C2] border text-white hover:bg-white hover:text-[#393ECC] border-[#0065C2]",
+        outline: "bg-transparent border border-[#0065C2] text-[#0065C2]",
       },
       size: {
         sm: "py-2 px-6 text-base",
